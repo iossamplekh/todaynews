3e93d800e2a45d0497be5e064ae402d75798625f
rithlkh2 <rithronlkh@gmail.com>
2018-01-14 23:13:35 +0700

    add file customcell

---
+++ TodayNews/TodayNewsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TodayNewsTableViewCell.swift
+//  TodayNews
+//
+//  Created by Ron Rith on 1/14/18.
+//  Copyright © 2018 Ron Rith. All rights reserved.
+//
+
+import UIKit
+
+class TodayNewsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
